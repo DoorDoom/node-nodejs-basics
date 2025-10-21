ea4cff65c34517df66a667cd2bee89249987c3b5
Alina Vagner <ali.vag977@gmail.com>
2025-10-21 19:13:24 +0300

    add create function code

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-  // Write your code here
+  const data = "I am fresh and young";
+
+  try {
+    await writeFile("./src/fs/files/fresh.txt", data, { flag: "wx+" });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
