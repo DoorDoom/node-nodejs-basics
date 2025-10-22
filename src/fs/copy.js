5a095936edf5058ef9477f2364f9dfaa315f0217
Alina Vagner <ali.vag977@gmail.com>
2025-10-22 11:27:15 +0300

    create copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-  // Write your code here
+  try {
+    await cp("./src/fs/files", "./src/fs/files_copy", {
+      force: false,
+      errorOnExist: true,
+      recursive: true,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
