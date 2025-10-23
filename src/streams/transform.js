639d7e3438554c6fe7812b4f152462f57642b989
Alina Vagner <ali.vag977@gmail.com>
2025-10-23 20:27:12 +0300

    create transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { pipeline } from "node:stream/promises";
+import { createReadStream } from "node:fs";
+import { stdin, stdout } from "node:process";
+import { Transform } from "node:stream";
+
 const transform = async () => {
-  // Write your code here
+  const revert = new Transform({
+    transform(data, _, cb) {
+      this.push(data.toString().split("").reverse().join(""));
+      cb();
+    },
+  });
+
+  await pipeline(stdin, revert, stdout);
 };
 
 await transform();
