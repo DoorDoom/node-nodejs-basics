7243c7d196dd67a4b6892162cd85d423a18f6112
Alina Vagner <ali.vag977@gmail.com>
2025-10-23 13:11:25 +0300

    change read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,4 @@
+import { pipeline } from "node:stream/promises";
 import { createReadStream } from "node:fs";
 import { stdout } from "node:process";
 
@@ -6,15 +7,19 @@ const read = async () => {
     encoding: "utf-8",
   });
 
-  readable.on("data", (chunk) => {
-    stdout.write(`${chunk}\n`);
-  });
+  // readable.on("data", (chunk) => {
+  //   stdout.write(`${chunk}\n`);
+  // });
   //can use for reading of the message in terminal, while the code process is alive
   // .on("close", () => {
   //   setTimeout(() => {
   //     stdout.write("close");
   //   }, 1000);
   // });
+
+  await pipeline(readable, async (source) => {
+    for await (const chunk of source) stdout.write(`${chunk}\n`);
+  });
 };
 
 await read();
