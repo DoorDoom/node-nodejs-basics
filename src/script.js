95ea9965f0fce4fefdbbe5be364e75384d275120
Alina Vagner <ali.vag977@gmail.com>
2025-11-01 15:31:50 +0300

    add basic operations

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
 import path from "node:path";
 import process, { chdir, stdout } from "node:process";
 import { pipeline } from "node:stream/promises";
-import { createReadStream } from "node:fs";
-import { readdir, stat, writeFile, mkdir as md } from "node:fs/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+import {
+  readdir,
+  stat,
+  writeFile,
+  mkdir as md,
+  rename,
+  rm,
+} from "node:fs/promises";
 import { homedir } from "node:os";
 
 let name =
@@ -17,7 +24,7 @@ console.log(`Welcome to the File Manager, ${name}!`);
 console.log(`You are currently in ${process.cwd()}`);
 
 process.stdin.on("data", async (chunk) => {
-  const [input, args] = chunk.toString().trim().split(" ");
+  const [input, ...args] = chunk.toString().trim().split(" ");
 
   try {
     switch (input) {
@@ -28,16 +35,28 @@ process.stdin.on("data", async (chunk) => {
         chdir(path.join(...process.cwd().split(path.sep).slice(0, -1)));
         break;
       case "cd":
-        findPath(args);
+        findPath(args[0]);
+        break;
+      case "cp":
+        await copyFile(args[0], args[1]);
+        break;
+      case "rn":
+        await renameFile(args[0], args[1]);
+        break;
+      case "rm":
+        await removeFile(args[0]);
+        break;
+      case "mv":
+        await moveFile(args[0], args[1]);
         break;
       case "cat":
-        await printContent(args);
+        await printContent(args[0]);
         break;
       case "add":
-        await createFile(args);
+        await createFile(args[0]);
         break;
       case "mkdir":
-        await createDirectory(args);
+        await createDirectory(args[0]);
         break;
       case "exit":
         process.exit(0);
@@ -87,6 +106,39 @@ const printContent = async (targetPath) => {
 
 const createFile = async (targetPath) => {
   await writeFile(targetPath, "", { flag: "wx+" });
+};
+
+const renameFile = async (oldName, newName) => {
+  await rename(oldName, newName, (err) => {
+    if (err) throw err;
+  });
+};
+
+const copyFile = async (sourcePath, targetPath) => {
+  const readable = createReadStream(sourcePath, {
+    encoding: "utf-8",
+  });
+  const writable = createWriteStream(targetPath, {
+    encoding: "utf-8",
+  });
+
+  await pipeline(readable, writable);
+};
+
+const moveFile = async (sourcePath, targetPath) => {
+  const readable = createReadStream(sourcePath, {
+    encoding: "utf-8",
+  });
+  const writable = createWriteStream(targetPath, {
+    encoding: "utf-8",
+  });
+
+  await pipeline(readable, writable);
+  await rm(sourcePath);
+};
+
+const removeFile = async (sourcePath) => {
+  await rm(sourcePath);
 };
 
 const createDirectory = async (targetPath) => {
